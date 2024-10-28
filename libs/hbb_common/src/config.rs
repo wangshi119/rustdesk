712f35c9ec2e1143a34893b5a94cff9dbeca184b
wangshi119 <8049176@qq.com>
2024-10-28 14:05:47 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["youku88.x3322.net"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &[youku88.x3322.net];
 pub const PUBLIC_RS_PUB_KEY: &str = "0ieNYv8YyCq7CTy7UBPDHBcajGJuu67cZlGldz71vEQ=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
