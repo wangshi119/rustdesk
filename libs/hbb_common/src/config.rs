5fd5681f0110b0c82fb675dc8a030397cbad3a08
wangshi119 <8049176@qq.com>
2024-10-29 14:09:02 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["youku88.x3322.net"];
-pub const PUBLIC_RS_PUB_KEY: &str = "0ieNYv8YyCq7CTy7UBPDHBcajGJuu67cZlGldz71vEQ=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.zonergy.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "LDli0KVz+F4Gn8oAOi8H8xXO7o6xAk2yH2czoJY09i8=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
